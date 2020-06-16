e24f13a21079336c494fde60a352a4cc60b87273
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 00:05:21 +0000

    character countered added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  console.log("hello!");
+  $("#tweet-text").keyup(function() {
+    const max = 140;
+    const char = $(this).val().length;
+    $(".counter").text(140 - char);
+    if (char >= max) {
+      $(".counter").css('color', 'red');
+    } else if (char < max) {
+      $(".counter").css('color', '#545149');
+    }
+  });
 });
