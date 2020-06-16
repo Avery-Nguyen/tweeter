dc0a1526672fcaadb81bf43bfdadd39c561e096b
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 14:57:52 +0000

    edited tweet style to fix screen better

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,7 +7,14 @@
 $(document).ready(function() {
   $("article").hover(function() {
     $(this).toggleClass('shadow');
-    $(this).find(".handle").hide();
+    $(this).find(".handle").show();
 
   });
+  $("article").hover(function () {
+    $(this).find(".handle").show();
+  }, 
+  function () {
+    $(this).find(".handle").hide();
+  }
+  );
 });
