49e1022551704f2095041788a3d34f4a960187b6
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 18:45:31 +0000

    function set-up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = /* Your code for creating the tweet element */
+  // ...
+  return $tweet;
+}
