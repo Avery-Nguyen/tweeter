7ab12c6eda25e7964b719c80a80f578715375a9c
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 20:53:55 +0000

    tweet rendering completed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,18 +8,36 @@
 
 $(document).ready(function() {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $('#tweets-container').append(newTweet);
+    }
   }
-  
   
   
   const createTweetElement = function(tweet) {
@@ -46,9 +64,6 @@ $(document).ready(function() {
     return $tweet
   }
   
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  console.log($('#tweets-container'));
+  renderTweets(data);
 
 });
