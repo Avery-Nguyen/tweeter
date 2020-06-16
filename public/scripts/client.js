87363d8be305892dd299eb2d7ae40efb84232274
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 18:41:34 +0000

    moved hover function to seperate js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,17 +4,3 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function() {
-  $("article").hover(function() {
-    $(this).toggleClass('shadow');
-    $(this).find(".handle").show();
-
-  });
-  $("article").hover(function () {
-    $(this).find(".handle").show();
-  }, 
-  function () {
-    $(this).find(".handle").hide();
-  }
-  );
-});
