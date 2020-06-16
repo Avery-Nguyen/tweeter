879661a426d1fb20b12eba30ff816fa061b1d0b6
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-16 03:42:11 +0000

    shawdow added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $("article").hover(function() {
+    $(this).toggleClass('shadow');
+    $(this).find(".handle").hide();
+
+  });
+});
