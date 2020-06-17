8349401b658496bac7a75ce86017320963165106
Avery Nguyen <avery.vnguyen@gmail.com>
2020-06-17 19:14:21 +0000

    ajax jquery post added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,32 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
+
+  //function to render created tweet onto page
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const newTweet = createTweetElement(tweet);
@@ -38,7 +40,7 @@ $(document).ready(function() {
     }
   }
   
-  
+  //create html template from the data
   const createTweetElement = function(tweet) {
     //time ago
     const current = new Date();
@@ -87,5 +89,20 @@ $(document).ready(function() {
   }
   
   renderTweets(data);
+
+  $('#post-tweet').submit((event) => {
+    event.preventDefault();
+    
+     const data = $(this).serialize();
+     console.log("data", data)
+
+    $.post('/tweets', data)
+
+    // $.getJSON('/tweets/')
+    // .then(function (data) {
+    // console.log("data", data)
+    // //add create tweet element and render functions
+    // });
+  });
 
 });
